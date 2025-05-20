3d11f3203efc771a7775d86998b0861ec91cfb48
akrrich <ayronovo@gmail.com>
2025-05-20 19:56:09 -0300

    Eliminacion de linea de codigo

--- Assets/Scripts/Managers/DeviceManager.cs
+++ Assets/Scripts/Managers/DeviceManager.cs
@@ -31,8 +31,6 @@ public class DeviceManager : MonoBehaviour
         IsJoystickUsed();
         IsMouseAndKeyboardUsed();
         EnabledAndDisabledCursor();
-
-        print(isUIModeActive);
     }
 
 
